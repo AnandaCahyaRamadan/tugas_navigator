4557dffc89c1ee2fbc84286420a133ef59d0f9f3
FlutLab.io <>
2022-05-30 22:10:55 +0000

    Autosave

--- lib/Features.dart
+++ lib/Features.dart
@@ -12,7 +12,7 @@ class Features extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 250,
+        height: 240,
         width: 220,
         child: ListView(
             scrollDirection: Axis.horizontal,
